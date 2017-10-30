c6de6ecfc7e8cd4ec15505a636f9cb5f12244921
rieck <rieck@mecom.de>
2017-10-30 12:30:50 +0100

    dont use hashes in names of resources copied by the file-loaderin order to make precaching easier

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -140,7 +140,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
-              name: 'static/media/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[ext]',
             },
           },
           // Process JS with Babel.
@@ -206,7 +206,7 @@ module.exports = {
             exclude: [/\.js$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
-              name: 'static/media/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[ext]',
             },
           },
         ],
